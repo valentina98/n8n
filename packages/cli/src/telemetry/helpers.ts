95c72d8e318d786f5b81cfe73b943b8ed327bca4
ahsan-virani <ahsan.virani@gmail.com>
2021-09-08 17:00:45 +0200

    wip: nodesGraph iteration

--- packages/cli/src/telemetry/helpers.ts
+++ packages/cli/src/telemetry/helpers.ts
@@ -1,9 +1,57 @@
 /* eslint-disable import/no-cycle */
-import { IDataObject } from 'n8n-workflow';
+import { IConnection, IDataObject, INode, INodeConnections } from 'n8n-workflow';
+import { URL } from 'url';
 import { WorkflowEntity } from '../databases/entities/WorkflowEntity';
 
-export async function generateNodesGraph(workflow: WorkflowEntity): Promise<IDataObject> {
-	return {
-		node_types: workflow.nodes.map((node) => node.type.split('.')[1]),
+interface INodesGraph {
+	node_types: string[];
+	node_connections: IDataObject[];
+	nodes: INodesGraphNode;
+}
+
+interface INodesGraphNode {
+	[key: string]: INodeItem;
+}
+
+interface INodeItem {
+	type: string;
+	domain?: string;
+}
+
+export async function generateNodesGraph(workflow: WorkflowEntity): Promise<INodesGraph> {
+	const nodesGraph: INodesGraph = {
+		node_types: [],
+		node_connections: [],
+		nodes: {},
 	};
+	const nodeNameAndInd: IDataObject = {};
+
+	workflow.nodes.forEach((node: INode, index: number) => {
+		nodesGraph.node_types.push(node.type);
+		const nodeItem: INodeItem = {
+			type: node.type,
+		};
+
+		if (node.type === 'n8n-nodes-base.httpRequest') {
+			const { hostname } = new URL(node.parameters.url as string);
+			nodeItem.domain = hostname;
+		}
+		nodesGraph.nodes[`${index}`] = nodeItem;
+		nodeNameAndInd[node.name] = index.toString();
+	});
+
+	const getGraphConnectionItem = (startNode: string, connectionItem: IConnection) => {
+		return { start: nodeNameAndInd[startNode], end: nodeNameAndInd[connectionItem.node] };
+	};
+
+	Object.keys(workflow.connections).forEach((nodeName) => {
+		const connections: INodeConnections = workflow.connections[nodeName];
+		connections.main.forEach((element: IConnection[]) => {
+			element.forEach((element2) => {
+				nodesGraph.node_connections.push(getGraphConnectionItem(nodeName, element2));
+			});
+		});
+	});
+
+	return nodesGraph;
 }
